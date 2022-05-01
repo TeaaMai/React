b46a57d78e8a7024c88e24e356468e16a1c4f061
TeaaMai <94010817+TeaaMai@users.noreply.github.com>
2022-04-30 19:25:26 -0500

    Update App.js

--- contador/src/App.js
+++ contador/src/App.js
@@ -1,14 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+
+  const[inicio, sumar] = useState(0); 
+
+  const suma = () =>{
+    try {
+      sumar(inicio+1)
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const resta = () =>{
+    try {
+      
+      sumar(inicio-1)
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Contador: {inicio}
         </p>
+        <button onClick={suma}>Sumar 1 </button>
+        <br></br>
+        <button onClick={resta}> Resta 1</button>
+        <br></br>
         <a
           className="App-link"
           href="https://reactjs.org"
